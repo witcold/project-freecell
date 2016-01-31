80661fe5b7ffb68661d40dffb91b6ca278cab2f5
Witold Mescheryakov <wmescheryakov@gmail.com>
2016-01-31 16:35:56 +0300

    Suit and Value became enums, added demo shuffle

--- classes.py
+++ classes.py
@@ -1,41 +1,71 @@
 # coding=utf-8
+from enum import Enum
+from random import shuffle
 
 
-class Suit(object):
-    SPADES = 1
-    CLUBS = 2
-    DIAMONDS = 3
-    HEARTS = 4
-    icons = ['♠', '♣', '♦', '♥']
-    blank = ['♤', '♧', '♢', '♡']
+class Suit(Enum):
+    __order__ = 'SPADES CLUBS DIAMONDS HEARTS'
+    SPADES = '♠'
+    CLUBS = '♣'
+    DIAMONDS = '♦'
+    HEARTS = '♥'
 
 
-class Value(object):
-    ACE = 1
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-    icons = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+class Value(Enum):
+    __order__ = 'ACE TWO THREE FOUR FIVE SIX SEVEN EIGHT NINE TEN JACK QUEEN KING'
+    ACE = 'A'
+    TWO = '2'
+    THREE = '3'
+    FOUR = '4'
+    FIVE = '5'
+    SIX = '6'
+    SEVEN = '7'
+    EIGHT = '8'
+    NINE = '9'
+    TEN = '10'
+    JACK = 'J'
+    QUEEN = 'Q'
+    KING = 'K'
 
 
 class Card(object):
     def __init__(self, suit, value):
+        """Creates a Card.
+
+        :type suit: Suit
+        :type value: Value
+        :rtype: Card
+        """
         self.suit = suit
         self.value = value
 
     def __repr__(self):
-        return Value.icons[self.value] + Suit.icons[self.suit]
+        return self.value.value + self.suit.value
 
 
 class Stack(object):
-    def __init__(self, *cards):
-        self.cards = cards
+    def __init__(self):
+        self.cards = []
+
+    def __getitem__(self, item):
+        return self.cards.__getitem__(item)
+
+    def __iter__(self):
+        return self.cards.__iter__()
+
+    def __len__(self):
+        return self.cards.__len__()
+
+    def append(self, card):
+        self.cards.append(card)
+
+
+class Deck(Stack):
+    def __init__(self):
+        self.cards = []
+        for suit in Suit:
+            for value in Value:
+                self.cards.append(Card(suit, value))
+
+    def shuffle(self):
+        shuffle(self.cards)
--- free_cell.py
+++ free_cell.py
@@ -1,5 +1,7 @@
 # coding=utf-8
-from classes import Stack
+from itertools import izip
+from classes import Stack, Deck
+from utils import chunks
 
 
 class Foundation(Stack):
@@ -18,11 +20,30 @@ class Game(object):
     def __init__(self):
         self.cells = []
         for _ in xrange(4):
-            self.cells += Cell()
+            self.cells.append(Cell())
         self.foundations = []
         for _ in xrange(4):
-            self.foundations += Foundation()
+            self.foundations.append(Foundation())
         self.cascades = []
         for _ in xrange(8):
-            self.cascades += Cascade()
+            self.cascades.append(Cascade())
         # TODO fill cascades with cards
+        deck = Deck()
+        deck.shuffle()
+        for chunk in chunks(deck, 8):
+            for cascade, card in izip(self.cascades, chunk):
+                cascade.append(card)
+
+    def show(self):
+        depths = [len(x) for x in self.cascades]
+        max_depth = max(depths)
+        for i in xrange(max_depth):
+            for j, cascade in enumerate(self.cascades):
+                if i < depths[j]:
+                    print cascade[i],
+                else:
+                    print '  ',
+            print
+
+if __name__ == '__main__':
+    Game().show()
